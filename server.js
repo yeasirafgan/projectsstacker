05aa0daf2e9757d9f996861657c25a8ef0774173
Sher yeasir afgan <yeasir.one@gmail.com>
2023-12-17 21:39:35 +0000

    mongo connected

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require('express');
+const mongoose = require('mongoose');
 const projectRoutes = require('./routes/projectRoute')
 // express app
 const app = express();
@@ -19,7 +20,14 @@ app.use((req,res,next)=> {
 // routes
 app.use('/api/projects', projectRoutes)
 
-// listen to port
+// mongodb
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+  // listen to port
 app.listen(port,()=>{
-  console.log(`listening on http://localhost:${port}`)
+  console.log(` Connected to mongo and listening on http://localhost:${port}`)
 })
+}).catch((err)=>{
+  console.log(err)
+})
+
+
